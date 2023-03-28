fce2708540f2cd1ea8606232ed595b32cc46a02f
srinivassomesetty9 <srinivassomesetty@gmail.com>
2023-03-28 17:32:20 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-main">
+      <Header />
+      <Contacts />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Header.css"
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarwrapper">
+        <div className="topleft">
+          <span className='logo'>Contacts</span>
+        </div>
+        <div className="topright">
+          <div className="topbarIcons">
+          
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
+          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="16" fill="white" className="bi bi-gear-fill" viewBox="0 0 16 16">
+            <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+          </svg>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
